434327c5ccc68e185e9d494270b7eb24f0b63f51
JeongYunha <yunhadev98@gmail.com>
2022-03-12 15:10:44 +0900

    upload project

---
+++ src/main/java/com/csbot/admin/csbotadmin/CsBotAdminApplication.java
@@ -0,0 +1,13 @@
+package com.csbot.admin.csbotadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsBotAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsBotAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csbot/admin/csbotadmin/CsBotAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csbot.admin.csbotadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsBotAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
